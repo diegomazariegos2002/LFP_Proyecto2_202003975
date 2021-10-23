d8b382f549234a07892656f937ea96228a241f6e
Diego Mazariegos <dmazariegosbarrientos@gmail.com>
2021-10-23 11:17:08 -0600

    funcionalidad base añadida

--- prueba.py
+++ prueba.py
@@ -1,8 +1,43 @@
-diccionario = {}
+datos = []
+lista_Claves = []
+lista_Registros = []
+lista_Claves.append('hola')
+lista_Claves.append('adios1')
+lista_Claves.append('adios2')
+lista_Claves.append('adios3')
+datos.append(lista_Claves)
+lista_Registros.append(1)
+lista_Registros.append(2)
+lista_Registros.append(3)
+lista_Registros.append(4)
+datos.append(list(lista_Registros))
+lista_Registros = []
+lista_Registros.append(4)
+lista_Registros.append(3)
+lista_Registros.append(2)
+lista_Registros.append(1)
+datos.append(list(lista_Registros))
+lista_Registros = []
+lista_Registros.append(5)
+lista_Registros.append(6)
+lista_Registros.append(7)
+lista_Registros.append(8)
+datos.append(list(lista_Registros))
+print(datos)
 
-diccionario.setdefault('a')
+print(datos[1][0], datos[2][0], datos[3][0])
 
-print(diccionario)
-diccionario.values()
-print(diccionario['a'])
-print(diccionario.keys())
+
+busquedaCol = "adios1"
+index = 0
+for item in datos[0]:
+    if item == busquedaCol:
+        break
+    index += 1
+cont = 1
+suma = 0
+while(cont <= (len(datos)-1)):
+    suma += datos[cont][index]
+    cont+=1
+
+print(suma)
